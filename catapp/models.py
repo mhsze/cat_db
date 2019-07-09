a275e1f1348d4a18bf5133b4ec97791e99d4ed3f
mhsze <munhou.sze@gmail.com>
2019-07-09 15:09:47 +0800

    Added pre-commit and pytest

--- catapp/models.py
+++ catapp/models.py
@@ -1,33 +1,30 @@
 from django.db import models
 
 # Create your models here.
-GENDER_CHOICES = (
-    ('M', 'Male'),
-    ('F', 'Female'),
-)
+GENDER_CHOICES = (("M", "Male"), ("F", "Female"))
+
 
 class Home(models.Model):
-    HOME_CHOICES = (
-    ('LANDED', "Landed"),
-    ('CONDO', "Condominium"),
-    )
+    HOME_CHOICES = (("LANDED", "Landed"), ("CONDO", "Condominium"))
 
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
-    type = models.CharField(choices=HOME_CHOICES, default='Landed', max_length=100)
+    type = models.CharField(choices=HOME_CHOICES, default="Landed", max_length=100)
 
     def __str__(self):
         return self.name
+
 
 class Human(models.Model):
     name = models.CharField(max_length=255)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     date_of_birth = models.DateField(null=True, blank=True)
     description = models.TextField(blank=True)
-    home = models.ForeignKey(Home, related_name='humans', on_delete=models.CASCADE)
+    home = models.ForeignKey(Home, related_name="humans", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
 
 class Breed(models.Model):
     name = models.CharField(max_length=255)
@@ -37,13 +34,14 @@ class Breed(models.Model):
     def __str__(self):
         return self.name
 
+
 class Cat(models.Model):
     name = models.CharField(max_length=255)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     date_of_birth = models.DateField(null=True, blank=True)
     description = models.TextField(blank=True)
-    breed = models.ForeignKey(Breed, related_name='cats', on_delete=models.CASCADE)
-    owner = models.ForeignKey(Human, related_name='cats', on_delete=models.CASCADE)
+    breed = models.ForeignKey(Breed, related_name="cats", on_delete=models.CASCADE)
+    owner = models.ForeignKey(Human, related_name="cats", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
