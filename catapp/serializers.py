24712549b5e2b892a6dbad79976fa61881e29753
mhsze <munhou.sze@gmail.com>
2019-07-11 12:45:29 +0800

    added id to API output

--- catapp/serializers.py
+++ catapp/serializers.py
@@ -1,45 +1,73 @@
-#from exceptions import AttributeError
+# from exceptions import AttributeError
+
+from rest_framework import serializers
+from rest_framework.serializers import HyperlinkedModelSerializer
 
 from catapp.models import GENDER_CHOICES
-from rest_framework import serializers
-from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
+
 from .models import Breed, Cat, Home, Human
+
 
 class GenderChoiceField(serializers.ChoiceField):
     def to_representation(self, obj):
         return self._choices[obj]
 
+
 class HomeListingField(serializers.RelatedField):
     def to_representation(self, value):
         human = Human.objects.get(id=value.owner_id)
         home = Home.objects.get(id=human.home_id)
-        return 'Cat: %s, Home: %s' % (value.name, home)
+        return "Cat: %s, Home: %s" % (value.name, home)
+
 
 class HomeSerializer(HyperlinkedModelSerializer):
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Home
-        fields = '__all__'
+        fields = "__all__"
+
 
 class HumanSerializer(HyperlinkedModelSerializer):
-    cats = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='cat-detail')
-    #cats_name = serializers.StringRelatedField(many=True, read_only=True, source='cats')
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
+    cats = serializers.HyperlinkedRelatedField(
+        many=True, read_only=True, view_name="cat-detail"
+    )
+    # cats_name = serializers.StringRelatedField(many=True, read_only=True, source='cats')
     gender = GenderChoiceField(choices=GENDER_CHOICES)
+
     class Meta:
         model = Human
-        fields = '__all__'
+        fields = "__all__"
+
 
 class BreedSerializer(HyperlinkedModelSerializer):
-    homes = HomeListingField(many=True, source='cats', read_only=True)
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
+    homes = HomeListingField(many=True, source="cats", read_only=True)
+
     class Meta:
         model = Breed
-        fields = '__all__'
+        fields = "__all__"
+
 
 class CatSerializer(HyperlinkedModelSerializer):
-    #gender = serializers.ChoiceField(choices=GENDER_CHOICES)
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
+    # gender = serializers.ChoiceField(choices=GENDER_CHOICES)
     gender = GenderChoiceField(choices=GENDER_CHOICES)
-    home = serializers.SerializerMethodField('owner_home')
+    home = serializers.SerializerMethodField("owner_home")
+
     def owner_home(self, cat):
         return cat.owner.home.name
+
     class Meta:
         model = Cat
-        fields = ('url', 'name', 'gender', 'date_of_birth', 'description', 'breed', 'owner', 'home')
+        fields = (
+            "url",
+            "name",
+            "gender",
+            "date_of_birth",
+            "description",
+            "breed",
+            "owner",
+            "home",
+        )
